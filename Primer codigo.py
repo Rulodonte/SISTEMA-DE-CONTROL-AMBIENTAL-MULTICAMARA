ab7840367e92e451df4e767c0be4a56ac5f97497
Rulodonte <raul.rodriguez.ruelas@gmail.com>
2023-09-12 22:25:07 -0600

    Segunda modificacion "Se agrego codigo para resolver la hipotenusa"

--- Primer codigo.py
+++ Primer codigo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 # Crear datos para la gráfica
 x = np.linspace(0, 2 * np.pi, 100)  # Valores de x de 0 a 2*pi
@@ -17,3 +18,15 @@ plt.show()
 
 # En esta parte agregamos la primera modificacion
 print("Primea modificacion")
+
+
+
+# Ingresar los valores de los catetos
+cateto1 = float(input("Ingresa la longitud del primer cateto: "))
+cateto2 = float(input("Ingresa la longitud del segundo cateto: "))
+
+# Calcular la hipotenusa usando el teorema de Pitágoras
+hipotenusa = math.sqrt(cateto1**2 + cateto2**2)
+
+# Mostrar el resultado
+print(f"La hipotenusa es: {hipotenusa}")
