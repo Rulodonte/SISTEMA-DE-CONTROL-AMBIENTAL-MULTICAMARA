49edc5b389d411824e3c6aea4cae1e9b7a4cccf6
Rulodonte <raul.rodriguez.ruelas@gmail.com>
2023-09-12 22:18:04 -0600

    Se crea el primer archivo python "Primer Codigo"

---
+++ Primer codigo.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Crear datos para la gráfica
+x = np.linspace(0, 2 * np.pi, 100)  # Valores de x de 0 a 2*pi
+y = np.sin(x)                      # Valores de y = sin(x)
+
+# Crear la gráfica
+plt.plot(x, y)
+plt.title('Gráfica de la función seno')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Mostrar la gráfica en pantalla
+plt.show()
